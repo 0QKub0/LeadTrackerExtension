227f1b2494119a0e3665661428e7205fe9e8c1f8
Mykyta <qkubplay@gmail.com>
2022-09-22 16:21:55 +0100

    refactored and upgraded

--- script.js
+++ script.js
@@ -2,23 +2,55 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const delBtn = document.getElementById("del-btn");
+const saveBtn = document.getElementById("save-btn");
+const delLastBtn = document.getElementById("del-last-btn");
 
-inputBtn.addEventListener("click",function(){
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    renderLeads();
-});
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("leads"));
 
-function renderLeads(){
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+function render(leads){
     let listItems = "";
-    for(let i = 0; i < myLeads.length; i++){
+    for(let i = 0; i < leads.length; i++){
         listItems += `
         <li>
-            <a href='${myLeads[i]}' target='_blank'> 
-                ${myLeads[i]}
+            <a href='${leads[i]}' target='_blank'> 
+                ${leads[i]}
             </a>
         </li>
     `;
     }
     ulEl.innerHTML = listItems;
 }
+
+saveBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("leads",JSON.stringify(myLeads));
+        render(myLeads);
+    
+    })
+})
+
+delBtn.addEventListener("click",function(){
+    myLeads = [];
+    localStorage.clear();
+    render(myLeads);
+})
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("leads",JSON.stringify(myLeads));
+    render(myLeads);
+});
+
+delLastBtn.addEventListener("click",function(){
+    myLeads.pop();
+    render(myLeads);
+})
+
